7e68756c9d9cb679043955d719f097956133b2c9
Shon Diaz <shondiaz@gmail.com>
2019-06-01 15:14:53 +0000

    save old code before c9 shutdown

--- polyjug.go
+++ polyjug.go
@@ -6,31 +6,33 @@ import (
 )
 
 type Jug struct {
-    DB *sql.DB
-    DriverName string
-    Path string
+	DB         *sql.DB
+	DriverName string
+	Path       string
 }
 
 func New(driverName string, path string) (*Jug, error) {
-    
+
 	// note: used for single transactions with reconnect (good for my slow polling)
-	return &Jug {
-	    DB: nil,
-	    Path: path,
+	return &Jug{
+		DB:   nil,
+		Path: path,
 	}, nil
 }
 
 func NewPersistant(driverName string, path string) (*Jug, error) {
 
-    // note: used when the connection should remain open between transactions
+	// note: used when the connection should remain open between transactions
 
 	db, err := sql.Open("sqlite3", path)
-	
-	if(err != nil){return nil, err}
-	
-	return &Jug {
-	    DB: db, //if this is set, then do not defer db.close
-	    Path: path,
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Jug{
+		DB:   db, //if this is set, then do not defer db.close
+		Path: path,
 	}, nil
 }
 
@@ -38,13 +40,17 @@ func (j *Jug) Query(query string) (resultSet []map[string]interface{}, err error
 
 	db, err := sql.Open(j.DriverName, j.Path)
 
-	if err != nil {return nil, err}
+	if err != nil {
+		return nil, err
+	}
 
 	defer db.Close()
 
 	rows, err := db.Query(query)
 
-	if(err != nil){return nil, err}
+	if err != nil {
+		return nil, err
+	}
 
 	records := make([]map[string]interface{}, 0)
 
@@ -57,13 +63,17 @@ func (j *Jug) Query(query string) (resultSet []map[string]interface{}, err error
 
 	for rows.Next() {
 
-		for i, _ := range headers { aRecordValuesPtrs[i] = &aRecordValues[i] }
+		for i, _ := range headers {
+			aRecordValuesPtrs[i] = &aRecordValues[i]
+		}
 
 		rows.Scan(aRecordValuesPtrs...)
 
 		thisRecord := make(map[string]interface{})
 
-		for i, fieldName := range headers { thisRecord[fieldName] = aRecordValues[i] }
+		for i, fieldName := range headers {
+			thisRecord[fieldName] = aRecordValues[i]
+		}
 
 		records = append(records, thisRecord)
 	}
@@ -71,28 +81,35 @@ func (j *Jug) Query(query string) (resultSet []map[string]interface{}, err error
 	return records, rows.Err()
 }
 
-
-func (j *Jug) Exec(query string, params ...interface{})(sql.Result ,error) {
+func (j *Jug) Exec(query string, params ...interface{}) (sql.Result, error) {
 
 	db, err := sql.Open(j.DriverName, j.Path)
 
-	if(err != nil) {return nil, err}
+	if err != nil {
+		return nil, err
+	}
 
 	defer db.Close()
 
 	transaction, err := db.Begin()
 
-	if(err != nil) {return nil, err}
+	if err != nil {
+		return nil, err
+	}
 
 	statement, err := transaction.Prepare(query)
 
-	if(err != nil){return nil, err}
+	if err != nil {
+		return nil, err
+	}
 
 	defer statement.Close()
 
 	res, err := statement.Exec(params...)
 
-	if(err != nil) {return res, err}
+	if err != nil {
+		return res, err
+	}
 
 	transaction.Commit()
 
@@ -100,6 +117,6 @@ func (j *Jug) Exec(query string, params ...interface{})(sql.Result ,error) {
 }
 
 //Headers with column type? for later casting?
-func (j *Jug) Header(tableName string) (headers map[string]string, err error){
+func (j *Jug) Header(tableName string) (headers map[string]string, err error) {
 	return map[string]string{}, nil
 }
